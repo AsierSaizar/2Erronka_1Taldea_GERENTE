995986486a1fe3d414dc6be9389a562b385fbd3a
AsierSaizar <asier.saizar.diaz@gmail.com>
2025-05-04 21:02:29 +0200

    euskera traduzituta

--- javafx/src/main/java/com/example/gerenteapp/ChatClient.java
+++ javafx/src/main/java/com/example/gerenteapp/ChatClient.java
@@ -22,8 +22,8 @@ public class ChatClient {
         public void connect() {
                 new Thread(() -> {
                         try {
-                                socket = new Socket("192.168.115.188", 9090);
-                                //socket = new Socket("127.16.0.1", 9090);
+                                //socket = new Socket("192.168.115.188", 9090);
+                                socket = new Socket("127.16.0.1", 9090);
                                 reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                                 writer = new PrintWriter(socket.getOutputStream(), true);
                                 listenForMessages();
